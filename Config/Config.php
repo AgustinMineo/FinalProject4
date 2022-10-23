5b3964af4176d0ca1728b18be497a5c41d83a7e4
AgustinMineo <60785051+AgustinMineo@users.noreply.github.com>
2022-10-23 11:39:57 -0300

    add folders and files

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+namespace Config;
+define("ROOT", dirname(__DIR__) . "/");
+define("FRONT_ROOT", "FinalProject4/");
+define("VIEWS_PATH", "Views/");
+define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
+define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+?>

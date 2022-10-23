98e3e9f4f7697d78c6d7e97756f0a15d651241bc
AgustinMineo <60785051+AgustinMineo@users.noreply.github.com>
2022-10-23 19:27:01 -0300

    dao´s implementation, views, etc

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,9 @@
 <?php
 namespace Config;
 define("ROOT", dirname(__DIR__) . "/");
-define("FRONT_ROOT", "FinalProject4/");
+define("FRONT_ROOT", "/FinalProject4/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+define("IMG_PATH", FRONT_ROOT.VIEWS_PATH . "img/");
 ?>
--- Models/User.php
+++ Models/User.php
@@ -1,2 +1,55 @@
 <?php
+namespace Models;
+
+abstract class User {
+    private $lastName;
+    private $firstName;
+    private $cellPhone;
+    private $birthDate;
+    private $email;
+    private $password;
+
+    // Getter and setters
+    public function getLastName(){
+        return $this->lastName;
+    }
+    public function setLastName($lastName){
+         $this->lastName = $lastName;
+    }
+
+    public function getfirstName(){
+        return $this->firstName;
+    }
+    public function setfirstName($firstName){
+        $this->firstName=$firstName;
+    }
+
+    public function getCellPhone(){
+        return $this->cellPhone;
+    }
+    public function setCellPhone($cellPhone){
+         $this->cellPhone = $cellPhone;
+    }
+
+    public function getbirthDate(){
+        return $this->birthDate;
+    }
+    public function setbirthDate($birthDate){
+         $this->birthDate = $birthDate;
+    }
+
+    public function getEmail(){
+        return $this->email;
+    }
+    public function setEmail($email){
+         $this->email = $email;
+    }
+
+    public function getPassword(){
+        return $this->password;
+    }
+    public function setPassword($password){
+         $this->password = $password;
+    }
+}
 ?>

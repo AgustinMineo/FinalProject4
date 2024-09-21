8110a6b823ca499225d448fa84526c2fbe25cc87
AgustinMineo <mineoagustin@gmail.com>
2024-09-21 20:30:33 -0300

    add 404 page, logic and change router to support the 404

--- Config/Router.php
+++ Config/Router.php
@@ -1,26 +1,37 @@
 <?php 
-    namespace Config;
+namespace Config;
 
-    use Config\Request as Request;
-
-    class Router
+use Config\Request as Request;
+use Helper\SessionHelper as SessionHelper;
+class Router
+{
+    public static function Route(Request $request)
     {
-        public static function Route(Request $request)
-        {
-            $controllerName = $request->getcontroller() . 'Controller';
+        $controllerName = $request->getcontroller() . 'Controller';
+        $methodName = $request->getmethod();
+        $methodParameters = $request->getparameters();          
 
-            $methodName = $request->getmethod();
+        $controllerClassName = "Controllers\\". $controllerName;            
 
-            $methodParameters = $request->getparameters();          
-
-            $controllerClassName = "Controllers\\". $controllerName;            
-
+        // Verificar si la clase del controlador existe
+        if (class_exists($controllerClassName)) {
             $controller = new $controllerClassName;
-            
-            if(!isset($methodParameters))            
-                call_user_func(array($controller, $methodName));
-            else
-                call_user_func_array(array($controller, $methodName), $methodParameters);
+
+            // Verificar si el método existe en el controlador
+            if (method_exists($controller, $methodName)) {
+                if (!isset($methodParameters) || empty($methodParameters)) {
+                    call_user_func(array($controller, $methodName));
+                } else {
+                    call_user_func_array(array($controller, $methodName), $methodParameters);
+                }
+            } else {
+                // Redirigir al login si el método no existe
+                SessionHelper::redirectTo404();
+            }
+        } else {
+            // Redirigir al login si el controlador no existe
+            SessionHelper::redirectTo404();
         }
     }
-?>
+
+}
